6494962c7389642138fa0e87efcc46cb3f5a4bc0
Hongyu Cai <h.tsai@hotmail.com>
2023-05-23 20:40:58 -0400

    gui

--- gui.py
+++ gui.py
@@ -17,16 +17,17 @@ from modules.utils import depth2img
 
 warnings.filterwarnings("ignore")
 
+
 @ti.kernel
-def write_buffer(W: ti.i32, H: ti.i32, x: ti.types.ndarray(),
-                 final_pixel: ti.template()):
+def write_buffer(
+    W: ti.i32, H: ti.i32, x: ti.types.ndarray(), final_pixel: ti.template()
+):
     for i, j in ti.ndrange(W, H):
         for p in ti.static(range(3)):
             final_pixel[i, j][p] = x[H - j, i, p]
 
 
 class OrbitCamera:
-
     def __init__(self, K, img_wh, poses, r):
         self.K = K
         self.W, self.H = img_wh
@@ -63,28 +64,21 @@ class OrbitCamera:
     def orbit(self, dx, dy):
         rotvec_x = self.rot[:, 1] * np.radians(100 * self.rotate_speed * dx)
         rotvec_y = self.rot[:, 0] * np.radians(-100 * self.rotate_speed * dy)
-        self.rot = R.from_rotvec(rotvec_y).as_matrix() @ \
-                   R.from_rotvec(rotvec_x).as_matrix() @ \
-                   self.rot
+        self.rot = (
+            R.from_rotvec(rotvec_y).as_matrix()
+            @ R.from_rotvec(rotvec_x).as_matrix()
+            @ self.rot
+        )
 
     def scale(self, delta):
-        self.radius *= 1.1**(-delta)
+        self.radius *= 1.1 ** (-delta)
 
     def pan(self, dx, dy, dz=0):
         self.center += 1e-4 * self.rot @ np.array([dx, dy, dz])
 
 
 class NGPGUI:
-
-    def __init__(
-            self, 
-            hparams, 
-            model_config, 
-            K, 
-            img_wh, 
-            poses, 
-            radius=4.5
-        ):
+    def __init__(self, hparams, model_config, K, img_wh, poses, radius=4.5):
         self.hparams = hparams
         self.model = NGP(**model_config).cuda()
 
@@ -96,13 +90,9 @@ class NGPGUI:
 
         self.cam = OrbitCamera(K, img_wh, poses, r=radius)
         self.W, self.H = img_wh
-        self.render_buffer = ti.Vector.field(
-            n=3,
-            dtype=float,
-            shape=(self.W, self.H)
-        )
+        self.render_buffer = ti.Vector.field(n=3, dtype=float, shape=(self.W, self.H))
 
-        if self.hparams.dataset_name in ['colmap', 'nerfpp']:
+        if self.hparams.dataset_name in ["colmap", "nerfpp"]:
             self.exp_step_factor = 1 / 256
         else:
             self.exp_step_factor = 0
@@ -114,21 +104,15 @@ class NGPGUI:
 
     def render_cam(self):
         t = time.time()
-        with torch.autocast(device_type='cuda', dtype=torch.float16):
+        with torch.autocast(device_type="cuda", dtype=torch.float16):
             directions = get_ray_directions(
-                self.cam.H,
-                self.cam.W,
-                self.cam.K,
-                device='cuda'
+                self.cam.H, self.cam.W, self.cam.K, device="cuda"
             )
-            rays_o, rays_d = get_rays(
-                directions,
-                torch.cuda.FloatTensor(self.cam.pose)
-            )
+            rays_o, rays_d = get_rays(directions, torch.cuda.FloatTensor(self.cam.pose))
             results = render(
-                self.model, 
-                rays_o, 
-                rays_d, 
+                self.model,
+                rays_o,
+                rays_d,
                 test_time=True,
                 exp_step_factor=self.exp_step_factor,
             )
@@ -137,7 +121,7 @@ class NGPGUI:
         depth = rearrange(results["depth"], "(h w) -> h w", h=self.H)
         # torch.cuda.synchronize()
         self.dt = time.time() - t
-        self.mean_samples = results['total_samples'] / len(rays_o)
+        self.mean_samples = results["total_samples"] / len(rays_o)
 
         if self.img_mode == 0:
             return rgb
@@ -161,56 +145,35 @@ class NGPGUI:
         return last_orbit_x, last_orbit_y
 
     def check_key_press(self, window):
-        if window.is_pressed('w'):
+        if window.is_pressed("w"):
             self.cam.scale(0.2)
-        if window.is_pressed('s'):
+        if window.is_pressed("s"):
             self.cam.scale(-0.2)
-        if window.is_pressed('a'):
-            self.cam.pan(100, 0.)
-        if window.is_pressed('d'):
-            self.cam.pan(-100, 0.)
-        if window.is_pressed('e'):
-            self.cam.pan(0., -100)
-        if window.is_pressed('q'):
-            self.cam.pan(0., 100)
+        if window.is_pressed("a"):
+            self.cam.pan(100, 0.0)
+        if window.is_pressed("d"):
+            self.cam.pan(-100, 0.0)
+        if window.is_pressed("e"):
+            self.cam.pan(0.0, -100)
+        if window.is_pressed("q"):
+            self.cam.pan(0.0, 100)
 
     def render(self):
-
         window = ti.ui.Window(
-            'taichi_ngp',
+            "Hongyu Cai: Neural Radiance Field",
             (self.W, self.H),
         )
         canvas = window.get_canvas()
-        gui = window.get_gui()
 
         # GUI controls variables
         last_orbit_x = None
         last_orbit_y = None
 
-        view_id = 0
-        last_view_id = 0
-
-        views_size = self.poses.shape[0] - 1
-
         while window.running:
             self.check_key_press(window)
             last_orbit_x, last_orbit_y = self.check_cam_rotate(
-                window, last_orbit_x, last_orbit_y)
-            with gui.sub_window("Control", 0.01, 0.01, 0.4, 0.2) as w:
-                self.cam.rotate_speed = w.slider_float('rotate speed',
-                                                       self.cam.rotate_speed,
-                                                       0.1, 1.)
-
-                self.img_mode = w.checkbox("show depth", self.img_mode)
-
-                view_id = w.slider_int('train view', view_id, 0, views_size)
-
-                if last_view_id != view_id:
-                    last_view_id = view_id
-                    self.cam.reset(self.poses[view_id])
-
-                w.text(f'samples per rays: {self.mean_samples:.2f} s/r')
-                w.text(f'render times: {1000*self.dt:.2f} ms')
+                window, last_orbit_x, last_orbit_y
+            )
 
             ngp_buffer = self.render_cam()
             write_buffer(self.W, self.H, ngp_buffer, self.render_buffer)
